1ce6a0fbdc3b8468544f098c2edc9c598337d2c1
Michael Roberts <michael.roberts.jr217@gmail.com>
2019-11-08 15:00:57 -0500

    Added auth DB

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template, Blueprint, request
 import jwt
-import sqlalchemy, pymysql
+import sqlalchemy as db
+import pymysql
 
 app = Flask(__name__)
+
 app.config['SECRET'] = "XCAP05H6LoKvbRRa/QkqLNMI7cOHguaRyHzyg7n5qEkGjQmtBhz4SzYh4Fqwjyi3KJHlSXKPwVu2+bXr6CtpgQ=="
-app.config['DB_URL'] = ""
+app.config['DB_HOST'] = "35.202.171.233"
+app.config['DB_USER'] = "admin"
+app.config['DB_PASS'] = "team6pass"
+app.config['DB_NAME'] = "users"
+
+#engine = db.create_engine('mysql+pymysql://' + app.config['DB_USER'] + ':' + app.config['DB_PASS'] + '@/' + app.config['DB_NAME'] + '/inlaid-goods-257500:us-central1:mysql-test', pool_pre_ping=True)
+
+#app.config['DB_CONN'] = engine.connect()
 
 @app.route('/')
 def home():
     return render_template("obs_navigation.html")
 
-@app.route('/login')
-def login():
-    encoded = jwt.encode({'some': 'payload'}, app.config['SECRET'], algorithm='HS256')
-    return render_template("obs_login.html")
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        username = request.header.username
+        password = request.header.password
+        email = request.header.email
+        
+
+        #app.config['DB_CONN'].execute('INSERT INTO users VALUES(' + username + ', ' + password + ', ' + email + ')')
+
+        #encoded = jwt.encode({'some': 'payload'}, app.config['SECRET'], algorithm='HS256')
+
+        return "Success!"
+
+
+
+
 
 @app.route('/welcome')
 def welcome():
@@ -21,4 +46,4 @@ def welcome():
 
 if __name__ == "__main__" :
     
-    app.run()
+    app.run(debug=True)
